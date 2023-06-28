92054910f7191efb0638f1bc3a5f6b5ca05cf1e2
david <davidmarquez007@hotmail.com>
2023-06-28 10:42:26 -0500

    Estructurando clase Libro con sus atributos y metodos getters y setters.

---
+++ src/Libro.java
@@ -0,0 +1,39 @@
+public class Libro {
+
+    private String titulo;
+    private String autor;
+    private String genero;
+    private short anhoPublicacion;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public short getAnhoPublicacion() {
+        return anhoPublicacion;
+    }
+
+    public void setAnhoPublicacion(short anhoPublicacion) {
+        this.anhoPublicacion = anhoPublicacion;
+    }
+}
